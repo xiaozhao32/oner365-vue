87b83c4bd1f8c52ded31e0763763f4e15f55dce1
zhaoyong <xiaozhao32@hotmail.com>
2021-12-11 16:20:59 +0800

    update

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -79,6 +79,8 @@ const user = {
           commit('SET_TOKEN', '')
           commit('SET_ROLES', [])
           commit('SET_PERMISSIONS', [])
+          commit('SET_NAME', '')
+          commit('SET_AVATAR', '')
           commit('SET_ID', '')
           removeToken()
           resolve()
@@ -91,7 +93,11 @@ const user = {
     // 前端 登出
     FedLogOut({ commit }) {
       return new Promise(resolve => {
-        commit('SET_TOKEN', '')
+        commit('SET_ROLES', [])
+        commit('SET_PERMISSIONS', [])
+        commit('SET_NAME', '')
+        commit('SET_AVATAR', '')
+        commit('SET_ID', '')
         removeToken()
         resolve()
       })
